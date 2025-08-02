6f1366cfc8f79a0b2fad6a89777aedc13e4fb5b0
webdizzy777 <carissa.mcmillian@gmail.com>
2025-08-02 11:06:28 -0500

    Goodbye World!

--- hello-world/src/main/java/org/example/Main.java
+++ hello-world/src/main/java/org/example/Main.java
@@ -3,10 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+
+        System.out.println("Goodbye World!");
     }
 }
